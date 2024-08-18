6d2141ec82d6fd53b6d90153e74f2858bd82fad1
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-08-18 00:44:15 -0300

    menu resposivo

--- javascript/script.js
+++ javascript/script.js
@@ -72,7 +72,42 @@ for(let i = 0; i<percent ; i++){
 //mix it up portfolio section 
 var mixer = mixitup('.portfolio-gallery');
 
+//Ativar menu contato section ///
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
 
 
+function activeMenu(){
+let len = section.length
+while(--len && window.scrollY + 97 < section[len].offsetTop){}
+menuLi.forEach(sec => sec.classList.remove("active"));
+menuLi[len].classList.add("active");
+
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+// section navbar ///
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky" ,window.scrollY > 50)
+});
 
 
+// section navbar ///
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+menuIcon.classList.toggle("bx-x");
+navlist.classList.toggle("open");
+
+}
+
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+
+    }
