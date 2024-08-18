9d493b75dac660743d8a34684515295af83b440b
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-08-18 18:56:28 -0300

    termino do responsivo

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
 let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
+words.forEach((word) => {
     let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
+    word.textContent = "";
+    letters.forEach((letter) => {
 
         let span = document.createElement("span");
         span.textContent = letter;
@@ -13,58 +13,58 @@ words.forEach((word)=>{
 });
 
 let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
+let maxWordIndex = words.length - 1;
 words[currentWordIndex].style.opacity = "1";
 
-let changeText = ()=>{
+let changeText = () => {
     let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
             letter.className = "letter out";
-        },i * 80);
+        }, i * 80);
 
     });
 
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter,i)=>{
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
         letter.className = "letter behind";
-        setTimeout(()=>{
+        setTimeout(() => {
 
             letter.className = "letter in";
 
-        },340 + i * 80);
+        }, 340 + i * 80);
 
     });
-currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 
 };
 
 changeText();
-setInterval(changeText,3000);
+setInterval(changeText, 3000);
 
 //circle skill//////////////////////////////////////////////////////////////////////////////////////////
 
 const circles = document.querySelectorAll(".circle");
-circles.forEach(elem=>{
-var dots = elem.getAttribute("data-dots");
-var marked = elem.getAttribute("data-percent");
-var percent = Math.floor(dots*marked/100);
-var points ="";
-var rotate = 360 / dots;
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots * marked / 100);
+    var points = "";
+    var rotate = 360 / dots;
 
-for(let i = 0 ; i < dots ; i++){
-    
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-}
+    for (let i = 0; i < dots; i++) {
 
-elem.innerHTML = points;
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
 
-const pointsMarked = elem.querySelectorAll(".points");
-for(let i = 0; i<percent ; i++){
-    pointsMarked[i].classList.add("marked");
-}
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll(".points");
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add("marked");
+    }
 
 
 })
@@ -77,21 +77,21 @@ let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
 
 
-function activeMenu(){
-let len = section.length
-while(--len && window.scrollY + 97 < section[len].offsetTop){}
-menuLi.forEach(sec => sec.classList.remove("active"));
-menuLi[len].classList.add("active");
+function activeMenu() {
+    let len = section.length
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
 
 }
 
 activeMenu();
-window.addEventListener("scroll",activeMenu);
+window.addEventListener("scroll", activeMenu);
 
 // section navbar ///
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky" ,window.scrollY > 50)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 50)
 });
 
 
@@ -99,15 +99,47 @@ window.addEventListener("scroll",function(){
 let menuIcon = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick = ()=>{
-menuIcon.classList.toggle("bx-x");
-navlist.classList.toggle("open");
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
 
 }
 
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navlist.classList.remove("open");
 
-    }
+}
+
+
+//parallax ///
+// Defina as variáveis primeiro
+const scrollScale = document.querySelectorAll(".scroll-scale");
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+const scrollTop = document.querySelectorAll(".scroll-top");
+
+// Crie o Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-items");
+        } else {
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+// Observe os elementos
+scrollScale.forEach((el) => observer.observe(el));
+scrollBottom.forEach((el) => observer.observe(el));
+scrollTop.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+
+
+
